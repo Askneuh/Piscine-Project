a80146a2e8d41b84e2141bbc7dad4b6bfd8cc8be
sebastien.pinta <sebastien.pinta@etu.umontpellier.fr>
2024-10-09 10:04:55 +0200

    implementation des fonctions estComplet et piocher

--- Sources/Joueur.swift
+++ Sources/Joueur.swift
@@ -55,5 +55,51 @@ struct Joueur : JoueurProtocol
         }
         return paquet
     }
+
+    mutating func piocher(i: Int, j: Int) -> Carte
+    /*
+    Renvoie la carte situé à l'indice de ligne i et de colonne j et la supprime dans la grille
+    entree :
+    i, j : Int, 0 <= i <= grille.count-1, 0 <= j <= grille.count-1
+    sortie : 
+    self.grille[i][j] : Carte : la carte situé à l'indice de ligne i et de colonne j
+    Precondition : la carte ne doit pas etre deja face visible
+    */
+    {   
+        if let c : Carte = self.grille[i][j]
+        {
+            self.grille[i][j] = nil
+            return c
+        }
+        //Ce cas ne devrait pas arriver
+        return Carte(numero : 0)
+    }
+
+
+    func estComplet() -> Bool
+    /*
+    Renvoie true si chacun des éléments de la grille est une carte, false s'il y a un nil dans la grille
+    Sortie :
+    res : bool
+    */
+    {
+        var i : Int = 0
+        var res : Bool = true
+        while i != self.grille.count && res == true
+        {
+            var j : Int = 0
+            while j != self.grille[i].count && res == true
+            {
+                if self.grille[i][j] == nil
+                {
+                    res = false
+                }
+                j += 1
+            }
+            i += 1
+        }
+        return res    
+    }
+
 }
 
