33ef6d5e667bef76f6de45e3b2466a1de2713173
Sebastien <askneuh@gmail.com>
2025-01-09 09:38:57 +0100

    small fix

--- Sources/Interface.swift
+++ Sources/Interface.swift
@@ -1,4 +1,6 @@
 func AffGrille(joueur : Joueur)
+//Fonction d'affichage de la grille d'un joueur passé en paramètre
+//Parcours chaque carte de la grille, si la carte est face cachée, affiche "?", sinon, affiche la valeur de la carte
 {
     for i in 0...joueur.grille.count-1
     {
@@ -23,6 +25,8 @@ func AffGrille(joueur : Joueur)
 }
 
 func affPaquet(paquet : [Carte?]) -> [Int]
+//Fonction d'affichage du paquet.
+//Ne sers pas dans le main, mais a permis de vérifier la création du paquet
 {
     var res : [Int] = [Int](repeating: 0, count: 100)
     for i in  0..<paquet.count{
@@ -39,6 +43,10 @@ func affPaquet(paquet : [Carte?]) -> [Int]
 }
 
 func demanderNbJoueur() -> Int
+//Fonction demandant à l'utilisateur d'insérer le nombre de joueur qu'il souhaite faire participer
+//La fonction vérifie que l'input soit bien un nombre entier compris entre 2 et 4, afin que le retour de cette fonction
+//soit conforme aux préconditions données par les règles du jeu
+//La fonction demande une entrée clavier en boucle jusqu'a obtenir une entrée correcte.
 {
     var commencerPartie : Bool = false
     var nbJoueur : Int = 0
@@ -70,6 +78,10 @@ func demanderNbJoueur() -> Int
 }
 
 func demanderNomJoueur(i : Int) -> String
+//Fonction demandant à l'utilisateur le nom du joueur créé en i-ième position par le jeu
+//La fonction demande une entrée clavier à l'utilisateur et la transforme en type String
+//Deux joueurs peuvent avoir le même nom
+//La fonction demande une entrée clavier en boucle jusqu'a obtenir une entrée correcte.
 {
     print("Nom du joueur", i, ": ")
     let nom: String? = String(readLine()!)
@@ -80,6 +92,10 @@ func demanderNomJoueur(i : Int) -> String
     return ""
 }
 func demanderIndice() -> (Int, Int){
+//Fonction qui demande à l'utilisateur les coordonnées d'une carte à enlever.
+//Elle vérifie que les indices entrés par l'utilisateur soit un nombre entier correspondent bien à une case de la grille
+//La fonction demande une entrée clavier en boucle jusqu'a obtenir une entrée correcte.
+//La fonction renvoie un tuple d'entier correspondant respectivement aux indices de ligne et de colonne.
     var isOK : Bool = false
     var i : Int = -1
     var j : Int = -1
@@ -120,6 +136,8 @@ func demanderIndice() -> (Int, Int){
 }
 
 func affCentre(centre: [Carte?]) {
+//Fonction qui permet l'affichage du centre du jeu
+//Pour chaque case du centre, si la case est une carte, la fonction affiche le numéro de la carte, sinon, la fonction affiche "/"
     for elt in centre {
         if let c: Carte = elt {
             print(c.numero, terminator: " ")
@@ -129,6 +147,7 @@ func affCentre(centre: [Carte?]) {
     }
 }
 func choisirCarteCentre(nbJoueur : Int, centre: [Carte?]) -> Int {
+    
     var isOK : Bool = false
     var i : Int = 0
     while !isOK{
--- Sources/main.swift
+++ Sources/main.swift
@@ -15,6 +15,31 @@ for numero in 1...10 {
 var partie : PartieProtocol = Partie(nbJoueur: nbJoueur, paquet: paquet)
 partie.distributionCarte()
 
+for i: Int in 0..<partie.ordrePassage.count {
+        print("Au tour de ", partie.ordrePassage[i].name)
+        print("\n")
+        AffGrille(joueur: partie.ordrePassage[i])
+        partie.placerAuCentre(k: i)
+        AffGrille(joueur: partie.ordrePassage[i])
+}
+partie.firstRoad()
+
+for k: Int in 0..<partie.ordrePassage.count {
+        print("Au tour de ", partie.ordrePassage[k].name)
+        print("\n")
+        AffGrille(joueur: partie.ordrePassage[k])
+        print("\n") 
+        affCentre(centre: partie.Centre)
+        var i: Int = choisirCarteCentre(nbJoueur: nbJoueur, centre: partie.Centre)
+        var carte : Carte = partie.retirerDuCentre(indice: i)
+        carte.retourner()
+        while !(partie.ordrePassage[k].estComplet()){
+            var dir : Direction = demanderDirection(joueur: partie.ordrePassage[k])
+            partie.ordrePassage[k].deplacer(deplacement: dir, carte: carte, i: partie.ordrePassage[k].coordCaseVide.0, j: partie.ordrePassage[k].coordCaseVide.1)
+            AffGrille(joueur: partie.ordrePassage[k])
+        }
+    }
+partie.changerOrdrePassage()
 
 
 for h in 0...15{
